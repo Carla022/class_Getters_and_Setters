111809ec04fbe62c092c713d1a74c373eec51fbc
Carla022 <cs.morenol@outlook.com>
2022-08-16 16:00:24 +0100

    added Cricle)

---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//       To find Circumference : 2 x 20 x 3.1415
+        Circle myCircle = new Circle(20);
+
+        System.out.println("Circle Radius: " + myCircle.getRadius());
+        System.out.println("Circle Circumference: " + myCircle.getCircumference());
+
+        myCircle.setRadius(8);
+
+        System.out.println("New Circle Radius: " + myCircle.getRadius());
+        System.out.println("New Circle Circumference: " + myCircle.getCircumference());
+
+
+//// Rectangle Exercise
+
+        Rectangle myRectangle = new Rectangle(1, 40);
+
+        System.out.println(myRectangle);
+
+
+
+
+
+
+
+
+
+
+//        System.out.println("Enter radius of circle: ");
+//        Scanner reader = new Scanner(System.in);
+//        double radius = reader.nextDouble();
+//
+//        double setRadius = Math.PI * radius * radius;
+//        System.out.println("Area = " +setRadius);
+    }
+
+
+
+
+
+
+
+    }
+
+
---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,28 @@
+public class Rectangle {
+
+    float length;
+    float width ;
+
+    public Rectangle (float length, float width) {
+        this.length = length;
+        this.width = width;
+
+    }
+    public float getLength(){
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+
+    }
+
+    public float getWidth (){
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+}
